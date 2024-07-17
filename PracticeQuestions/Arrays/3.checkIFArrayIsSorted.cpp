c5ed8bcf013eb9aca57b4bc58be1d9fc7a25a9c4
Devansh Kapoor <118839716+DevanshKapoor@users.noreply.github.com>
2024-07-18 03:21:51 +0530

    Add files via upload

---
+++ PracticeQuestions/Arrays/3.checkIFArrayIsSorted.cpp
@@ -0,0 +1,27 @@
+//check if given array is sorted
+#include<iostream>
+using namespace std;
+
+class Solution{
+    public:
+    bool isSorted(int arr[],int size){
+        for(int i=0;i<size;i++){
+            if(arr[i-1]>arr[i]){
+               return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main(){
+    Solution sol;
+    bool sorted1;
+    bool sorted2;
+    int arr[]={1,2,2,3,5,6,7,8};
+    int arr1[]={2,4,45,2,2,41,2};
+    sorted1=sol.isSorted(arr,8);
+    sorted2=sol.isSorted(arr1,7);
+    cout<<sorted1<<"\n";
+    cout<<sorted2;
+}
