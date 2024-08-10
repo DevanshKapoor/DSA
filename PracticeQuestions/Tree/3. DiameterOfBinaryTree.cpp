559f5226f49a6adc9416bad7de9a4b9de5e6b66f
DevanshKapoor <kapoordevansh368@gmail.com>
2024-08-10 15:52:14 +0530

    Create 3. DiameterOfBinaryTree.cpp

---
+++ PracticeQuestions/Tree/3. DiameterOfBinaryTree.cpp
@@ -0,0 +1,39 @@
+//maximum length of path between any 2 nodes of tree is diameter of tree.
+// length of path is nodes in path - 1.
+#include <iostream>
+using namespace std;
+//height of binary tree.
+//1+max(l+r).
+struct treeNode{
+    int data;
+    treeNode* right;
+    treeNode* left;
+    treeNode(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+    
+class Solution{
+    public:
+
+    int treeHeight(treeNode* root,int & diameter){
+        
+        if(root==NULL) return 0;
+        int lh=treeHeight(root->left,diameter);
+        int rh=treeHeight(root->right,diameter);
+        diameter=max(diameter,lh+rh);
+        return 1+max(lh,rh);
+    }    
+};
+
+int main() {
+    treeNode* root=new treeNode(1);
+    root->left=new treeNode(1);
+    root->right=new treeNode(1);
+    root->left->right=new treeNode(1);
+    Solution sol;
+    cout<<"maximum height is : "<<sol.treeHeight(root)<<endl;
+
+    return 0;
+}
