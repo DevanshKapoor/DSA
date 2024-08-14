416a9a7ddefc2cd11b6a6cb0ac2f96ee3f05b31f
DevanshKapoor <kapoordevansh368@gmail.com>
2024-08-15 05:27:50 +0530

    Create 2.DFSTraversal.cpp

---
+++ PracticeQuestions/Graph/2.DFSTraversal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+//0 based indexing
+
+vector<int> dfsTraversal(int n, vector<int> adj[]){
+   
+
+}
+
+int main(){
+
+    return 0;
+}
